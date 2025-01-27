71e6976810cd97341f556010283256d7097c377f
Henrique Meirelles das Dores Ferraz <hmeirelles0@gmail.com>
2025-01-27 19:45:52 -0300

    add connection to database
    Through the use of PHP Data Object (PDO), implemented connection to server database. Sends information regarding type, price, method of payment and store from which the transaction came.
    
    Added a SQL Query to the files for ease of tracking registered information on database.

---
+++ src/consulta_orcamento.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE orcamento;
+
+USE orcamento;
+
+CREATE TABLE tblCompras (
+	id_compra int not null identity primary key,
+	tipo_compra varchar(20) not null,
+	valor_compra dec(6, 2) not null,
+	local_compra varchar(50) not null,
+	metodo_compra varchar(20) not null
+);
+
+INSERT INTO tblCompras VALUES (
+	'Comida', 100.00, 'Casa de Bolo', 'PIX'
+);
+
+SELECT * FROM tblCompras;
+
+INSERT INTO tblCompras VALUES (
+	'Presentes', 200.00, 'Riachuelo', 'Crédito'
+);
+
+SELECT * FROM tblCompras WHERE metodo_compra = 'PIX';
--- src/planner.php
+++ src/planner.php
@@ -2,48 +2,80 @@
 
 /* PHP
 
-- Utiliza PDO (PHP Data Object) para preparar, substituir, executar e transferir as informações recebidas para um banco de dados;
-
 - Header() / location to return to homepage;
  */
 
+    //Inicializando variáveis de conexão ao banco de dados
+    $local_server = "ULTRON";
+    $usuario_server = "sa";
+    $senha_server = "Eldritch1890";
+    $banco_de_dados = "orcamento";
+    
+    try {
+        //Utilizando PDO (PHP Data Object)
+        $pdo = new PDO ("sqlsrv:server=$local_server;database=$banco_de_dados", $usuario_server, $senha_server);
+    } catch (Exception $ex) {
+        echo "Erro ao tentar conexão com o banco de dados:\n" . $ex->getMessage();
+        die; 
+    }
 
-    $tipo = $_POST['tipo'];
-    $valor = $_POST['valor'];
-    $local = $_POST['local'];
-    $metodo = $_POST['metodo'];
+    //Atribuição de tabela de interesse a uma variável
+    $compra = "tblCompras";
 
-    echo "
-        <body style= 'margin: 0%;text-align: center; background-image: linear-gradient(#10278f, #77db95);'>
-            <menu style = 'background-color:#03145e; display: flex; margin: 0px;'>
-                <nav style = 'color: white; background-color:rgb(28, 67, 241); padding: 3px; font-family: arial; border-radius: 8px;'><strong>RETORNAR</strong></nav>
-            </menu>
-            <h1 style = 'color: white; background-color: #03145e; margin: 0px 0px 0px 0px; padding-bottom: 20px;'>Registro atualizado com sucesso! &#x1F4B8;</h1>
-            <h2 style = 'color: white; background-color: #03145e; box-shadow: 0px 0px 20px 18px #03145e; margin: 0px 0px 10px 0px; padding-bottom: 10px;'>Detalhes:</h2>
-                <div style = 'width: fit-content; margin: auto; padding: 12% 0%;'>
-                    <table border style = 'background-color: white; text-align: center;'>
-                        <tr style = 'background-color: #10278f; '>
-                            <td style = 'padding: 3px;'>
-                                <strong style = 'color: white;'>Tipo de compra:</strong>
-                            </td>
-                            <td style = 'padding: 3px;'>
-                                <strong style = 'color: white;'>Valor da compra:</strong>
-                            </td>
-                            <td style = 'padding: 3px;'>
-                                <strong style = 'color: white;'>Local da compra:</strong>
-                            </td>
-                            <td style = 'padding: 3px;'>
-                                <strong style = 'color: white;'>Metodo da compra:</strong>
-                            </td>
-                        </th>
-                        <tr>
-                            <td><strong><ins>$tipo</ins></strong></td>
-                            <td><strong><ins>$valor</ins></strong></td>
-                            <td><strong><ins>$local</ins></strong></td>
-                            <td><strong><ins>$metodo</ins></strong></td>
-                        </tr>
-                    </table>
-                </div>
-        </body>
-    ";
+    try {
+        //Preparo da instrução SQL
+        $sql = $pdo->prepare('INSERT INTO ' . $compra . ' VALUES (:tipo, :valor, :local, :metodo);');
+        
+        $tipo = $_POST['tipo'];
+        $valor = $_POST['valor'];
+        $local = $_POST['local'];
+        $metodo = $_POST['metodo'];
+
+        //Associando valores (etapa necessária para maior segurança do sistema)
+        $sql->bindValue(":tipo", $tipo);
+        $sql->bindValue(":valor", $valor);
+        $sql->bindValue(":local", $local);
+        $sql->bindValue(":metodo", $metodo);
+
+        //Execução
+        $sql->execute();
+        
+        //Retorno visual da inclusão no banco de dados ao usuário
+        echo "
+            <body style= 'margin: 0%;text-align: center; background-image: linear-gradient(#10278f, #77db95);'>
+                <menu style = 'background-color:#03145e; display: flex; margin: 0px;'>
+                    <nav style = 'color: white; background-color:rgb(28, 67, 241); padding: 3px; font-family: arial; border-radius: 8px;'><strong>RETORNAR</strong></nav>
+                </menu>
+                <h1 style = 'color: white; background-color: #03145e; margin: 0px 0px 0px 0px; padding-bottom: 20px;'>Registro atualizado com sucesso! &#x1F4B8;</h1>
+                <h2 style = 'color: white; background-color: #03145e; box-shadow: 0px 0px 20px 18px #03145e; margin: 0px 0px 10px 0px; padding-bottom: 10px;'>Detalhes:</h2>
+                    <div style = 'width: fit-content; margin: auto; padding: 12% 0%;'>
+                        <table border style = 'background-color: white; text-align: center;'>
+                            <tr style = 'background-color: #10278f; '>
+                                <td style = 'padding: 3px;'>
+                                    <strong style = 'color: white;'>Tipo de compra:</strong>
+                                </td>
+                                <td style = 'padding: 3px;'>
+                                    <strong style = 'color: white;'>Valor da compra:</strong>
+                                </td>
+                                <td style = 'padding: 3px;'>
+                                    <strong style = 'color: white;'>Local da compra:</strong>
+                                </td>
+                                <td style = 'padding: 3px;'>
+                                    <strong style = 'color: white;'>Metodo da compra:</strong>
+                                </td>
+                            </th>
+                            <tr>
+                                <td><strong><ins>$tipo</ins></strong></td>
+                                <td><strong><ins>$valor</ins></strong></td>
+                                <td><strong><ins>$local</ins></strong></td>
+                                <td><strong><ins>$metodo</ins></strong></td>
+                            </tr>
+                        </table>
+                    </div>
+            </body>
+        ";
+    } catch (Exception $err) {
+        echo "Erro ao tentar uma inclusão de informações no banco de dados: \n" . $err->getMessage();
+        die;
+    }
 ?>
